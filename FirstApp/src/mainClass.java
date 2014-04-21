66bd03a67572f781c9267143d1c97508d9c5ac3c
cjswombc <cjswombc1@naver.com>
2014-04-21 10:23:04 +0900

    덧셈하는 거 이거야 오케이? Commit - Local repository Push - Remte repository 이름이 뭐에요? 전화번호 뭐에요?

---
+++ FirstApp/src/mainClass.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class mainClass {
+	public mainClass() {
+		System.out.println("안녕하세요^^;");
+		System.out.println();		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("첫번째 입력 : ");
+		int a = sc.nextInt();
+		System.out.print("두번째 입력 : ");
+		int b = sc.nextInt();
+		System.out.println();
+		int c = a + b;
+		System.out.print("답 : " + c);
+	}
+
+	public static void main(String[] args) {
+		mainClass mc = new mainClass();
+	}
+}
